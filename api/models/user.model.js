1a132970fcf9df40dc733df62e5e040a0f6e15e4
Jimoh Tajudeen <jimoh.tajudeen1017@gmail.com>
2024-02-19 02:06:31 +0100

    fixing the syntax err

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -1,24 +1,25 @@
-import mongoose, { Schema } from "mongoose";
+import mongoose from "mongoose";
 
-const userSchema = new Schema(
-    {
-        username:{
-            type: String,
-            require: true,
-            unique:true
-        },
-        email:{
-            type: String,
-            require: true,
-            unique:true
-        },
-        password:{
-            type: String,
-            require: true
-        }
-    }, {timestamps:true}
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      require: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      require: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      require: true,
+    },
+  },
+  { timestamps: true }
 );
 
 const User = mongoose.model("User", userSchema);
 
-export default User
+export default User;
